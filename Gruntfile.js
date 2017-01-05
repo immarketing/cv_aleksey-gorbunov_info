5215ecaa012c9080f17031dcba6bd2bec99e4280
immarketing <immarketing@mail.ru>
2017-01-05 22:45:09 +0300

    2222333

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,13 @@
 module.exports = function (grunt) { /*require('jit-grunt')(grunt);*/
     var globalConfig = {
+        releaseNo: '00.00.' + (Date.now()).toString(16).toLocaleUpperCase(),
+        releaseDate: '' + (new Date()).toString().toLocaleUpperCase(),
         images: 'images', /* папка для картинок сайта */
         styles: 'css', /* папка для готовый файлов css стилей */
         fonts: 'fonts', /* папка для шрифтов */
         scripts: 'js', /* папка для готовых скриптов js */
         src: 'src', /* папка с исходными кодами js, less , etc. */
+        distr: 'distr.tmp', /* папка для формирования дистрибутива. */
         bower_path: 'bower_components' /* папка где хранятся библиотеки jquery, bootstrap, SyntaxHighlighter, etc. */
     };
     /* Project configuration.*/
@@ -14,6 +17,7 @@ module.exports = function (grunt) { /*require('jit-grunt')(grunt);*/
         clean: {
             js: ['<%= globalConfig.scripts %>/*'],
             css: ['<%= globalConfig.styles %>/*'],
+            distr: ['<%= globalConfig.distr %>/**/*'],
             fonts: ['<%= globalConfig.fonts %>/*']
         },
         less: {
@@ -65,12 +69,150 @@ module.exports = function (grunt) { /*require('jit-grunt')(grunt);*/
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %>\n'
             },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= globalConfig.scripts %>',
+                    src: ['*.js', '!*.min.js'],
+                    //src: ['school.js', '!*.min.js'],
+                    dest: '<%= globalConfig.distr %>/<%= globalConfig.scripts %>'
+                }]
+            },
             build: {
                 src: 'js.src/cv.js',
                 dest: 'js/cv.min.js'
             }
         },
+
+        cssmin: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= globalConfig.styles %>',
+                    src: ['*.css', '!*.min.css'],
+                    dest: '<%= globalConfig.distr %>/<%= globalConfig.styles %>'
+                }]
+            }
+        },
+
+        htmlmin: {                                     // Task
+            dist: {                                      // Target
+                options: {                                 // Target options
+                    removeComments: true,
+                    minifyJS: true,
+                    minifyCSS: true,
+                    minifyURLs: true,
+                    collapseWhitespace: true
+                },
+                files: {                                   // Dictionary of files
+                    '<%= globalConfig.distr %>/index.html': 'index.html' // 'destination': 'source'
+                }
+            },
+            dev: {                                       // Another target
+                files: {
+                    //'dist/index.html': 'src/index.html',
+                    //'dist/contact.html': 'src/contact.html'
+                }
+            }
+        },
+
+        ftp_push: {
+            dist_cv_gorbunov_aleksey_ru: {
+                options: {
+                    //authKey: "serverA",
+                    username: "ftpcvalexeygorb",
+                    password: "o25Q45e34V22O68v",
+                    host: "cv.aleksey-gorbunov.info",
+                    dest: "/",
+                    port: 21
+                },
+                files: [
+                    {
+                        expand: true,
+                        //cwd: '.',
+                        cwd: '<%= globalConfig.distr %>/',
+                        src: [
+                            "index.html",
+                            '*.ver',
+                            "css/**",
+                            "fonts/**",
+                            "images/**",
+                            "js/**"
+                        ]
+                    }
+                ]
+            }
+        }
+        ,
+
+        gittag: {
+            dist: {
+                options: {
+                    // Target-specific options go here.
+                    force: true,
+                    annotated: true,
+                    tag: globalConfig.releaseNo,
+                    message: 'distr ' + globalConfig.releaseDate
+                }
+            }
+        },
+
+
+        "file-creator": {
+            dist: {
+                files: [
+                    {
+                        //file: "<%= globalConfig.distr %>/" + '<%= globalConfig.releaseNo  %>' + '.ver',
+                        file: "<%= globalConfig.distr %>/" + '.ver',
+                        method: function (fs, fd, done) {
+                            fs.writeSync(fd, globalConfig.releaseNo + '|' + globalConfig.releaseDate);
+                            done();
+                        }
+                    }
+                ]
+            }
+        },
+
         copy: {
+            dist: {
+                files: [
+                    /*
+                     {
+                     expand: true,
+                     flatten: true,
+                     cwd: '<%= globalConfig.scripts %>/',
+                     src: '*',
+                     dest: '<%= globalConfig.distr %>/<%= globalConfig.scripts %>/',
+                     filter: 'isFile'
+                     },
+                     {
+                     expand: true,
+                     flatten: true,
+                     cwd: '<%= globalConfig.styles %>/',
+                     src: '*',
+                     dest: '<%= globalConfig.distr %>/<%= globalConfig.styles %>/',
+                     filter: 'isFile'
+                     },
+                     */
+                    {
+                        expand: true,
+                        flatten: true,
+                        cwd: '<%= globalConfig.fonts %>/',
+                        src: '*',
+                        dest: '<%= globalConfig.distr %>/<%= globalConfig.fonts %>/',
+                        filter: 'isFile'
+                    }
+                    ,
+                    {
+                        expand: true,
+                        flatten: true,
+                        cwd: '<%= globalConfig.images %>/',
+                        src: '*',
+                        dest: '<%= globalConfig.distr %>/<%= globalConfig.images %>/',
+                        filter: 'isFile'
+                    }
+                ]
+            },
             main: {
                 files: [{
                     expand: true,
@@ -188,6 +330,12 @@ module.exports = function (grunt) { /*require('jit-grunt')(grunt);*/
     grunt.loadNpmTasks('grunt-responsive-images');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-ftp-push');
+
+    grunt.loadNpmTasks('grunt-git');
+
+    grunt.loadNpmTasks('grunt-file-creator');
 
     // Default task(s).
     // grunt.registerTask('default', [ 'uglify','less', 'watch' ]);
@@ -195,7 +343,18 @@ module.exports = function (grunt) { /*require('jit-grunt')(grunt);*/
     grunt.registerTask('default', ['clean', 'less', 'copy', 'uglify', 'responsive_images']);
     grunt.registerTask('imagetest', ['responsive_images']);
     //grunt.registerTask('serverdeploy', ['default', 'ftp-deploy']);
+    grunt.registerTask('prepareserverdeploy', ['default', 'copy:dist', 'htmlmin:dist', 'cssmin:dist', 'uglify:dist', 'file-creator:dist'
+        //, 'file-creator:dist'
+    ]);
+
+    grunt.registerTask('serverdeploy', ['prepareserverdeploy', 'gittag:dist', 'ftp_push:dist_cv_gorbunov_aleksey_ru']);
+
+
 
     // 11
 
 };
+
+
+
+
